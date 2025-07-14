132a878fac79a44f84034df025b0b3f6e6cc2f70
ritiksharmarj <theonlyritik@gmail.com>
2025-07-14 21:24:09 +0530

    feat: update App component content and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { ThemeProvider } from "./components/theme-provider";
+
 export default function App() {
   return (
     <ThemeProvider>
-      <div>hello</div>
+      <div>Enim qui ipsum pariatur ad excepteur fugiat ex dolor anim.</div>
     </ThemeProvider>
   );
 }
