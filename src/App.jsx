2fe962415500f6fcaf72f8b8798257803dc38d74
ritiksharmarj <theonlyritik@gmail.com>
2025-07-14 21:32:44 +0530

    feat: update routing and home page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./components/theme-provider";
+import HomePage from "./pages/home-page";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <div>Enim qui ipsum pariatur ad excepteur fugiat ex dolor anim.</div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
