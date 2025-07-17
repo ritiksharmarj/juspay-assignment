e602087086321a02ec5964680ff3df97d56c7358
ritiksharmarj <theonlyritik@gmail.com>
2025-07-18 04:49:30 +0530

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { ThemeProvider } from "./components/theme-provider";
 import { Layout } from "./components/ui/layout";
 import HomePage from "./pages/home-page";
 import PageNotFound from "./pages/not-found-page";
 import OrderListPage from "./pages/order-list-page";
 
+const router = createBrowserRouter([
+  {
+    Component: Layout,
+    children: [
+      { index: true, Component: HomePage },
+      { path: "order-list", Component: OrderListPage },
+    ],
+  },
+  {
+    path: "*",
+    Component: PageNotFound,
+  },
+]);
+
 export default function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/order-list" element={<OrderListPage />} />
-          </Route>
-
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
